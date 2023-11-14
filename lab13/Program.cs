e6544ce323e8281debcb36300b6bd5a43ad0fd44
PC GAMER <PC GAMER@CHARLES>
2023-11-14 16:08:39 -0500

    Agregar archivos de proyecto.

---
+++ lab13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lab13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                Console.Clear();
+                opcion = Pantallas.PantallaPrincipal();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Pantallas.Satisfaccion();
+                        break;
+                    case 2:
+                        Pantallas.VerDatosRegistrados();
+                        break;
+                    case 3:
+                        Pantallas.EliminarDato();
+                        break;
+                    case 4:
+                        // Agrega aquí la lógica para ordenar datos de menor a mayor
+                        //Aca no llego explicar y dijo que no lo hicieramos.
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (opcion != 5);
+        }
+    }
+}
+
+
